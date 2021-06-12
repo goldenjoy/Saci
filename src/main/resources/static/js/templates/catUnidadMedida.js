f73f819099417941885943e55805827d3c9e63aa
Michael Enrique <santiago.michael.es@gmail.com>
2021-06-11 23:47:42 -0500

    Tercer commit

--- src/main/resources/static/js/templates/catUnidadMedida.js
+++ src/main/resources/static/js/templates/catUnidadMedida.js
@@ -16,18 +16,27 @@ $( document ).ready(function() {
                 event.stopPropagation();
 
         		if (!(form.checkValidity() === false)) {
-          			const desc = {desc: $(form).find('#desc').val().trim()};
-                    const json = JSON.stringify(desc);
                     const actionType = $(form).find("#action-type").attr("action-type").trim();
+                    var desc;
+                    var id;
                     var type;
+                    var fJson;
+                    var json;
 
-                    //console.log("desc: " + $(form).find('#desc').val().trim());
-                    //console.log("action-type: " + actionType);
-
-                    if(actionType === "insert")
+                    if(actionType === "insert"){
+                        desc = $(form).find('#desc').val().trim();
                         type = "post";
-                    else if(actionType === "update")
+                        fJson = {desc: desc};
+                    }
+
+                    else if(actionType === "update"){
+                        desc = $(form).find('#desc').val().trim();
+                        id = $(form).find('#idItem').val().trim();
                         type = "put";
+                        fJson = {desc: desc, id: id};
+                    }
+
+                    json = JSON.stringify(fJson);
 
         			$.ajax({
 						type: type,
@@ -40,24 +49,33 @@ $( document ).ready(function() {
 				    	success: function(result){
 				    	    //console.log(result);
 
-                            var table = document.getElementById("tablaUnidadesMedida");
-                            if(parseInt(document.getElementById("cantList").value) === 0){
-                                table.deleteRow(1);
-                                document.getElementById("cantList").value = table.rows.length;
+                            if(actionType === "insert"){
+                                var table = document.getElementById("tablaUnidadesMedida");
+                                if(parseInt(document.getElementById("cantList").value) === 0){
+                                    table.deleteRow(1);
+                                    document.getElementById("cantList").value = table.rows.length;
+                                }
+                                var row = table.insertRow(table.rows.length);
+                                var cell0 = row.insertCell(0);
+                                var cell1 = row.insertCell(1);
+                                var cell2 = row.insertCell(2);
+                                var cell3 = row.insertCell(3);
+                                $(cell1).attr('id', 'tdDesc'+result.id);
+                                $(cell3).addClass('text-right');
+                                cell0.innerHTML = result.id;
+                                cell1.innerHTML = result.desc;
+                                cell2.innerHTML = "fecha";
+                                cell3.innerHTML =  `<button type="button" class="btn btn-primary btn-icon btn-sm" data-toggle="modal"
+                                                        onclick="$('#updateDescCatalogo').modal('toggle'); $('#idItem').val('` + result.id + `');"
+                                                        rel="tooltip" title="Actualizar la descripción">
+                                                        <i class="fa fa-pencil"></i>
+                                                    </button>`;
                             }
-                            var row = table.insertRow(table.rows.length);
-                            var cell0 = row.insertCell(0);
-                            var cell1 = row.insertCell(1);
-                            var cell2 = row.insertCell(2);
-                            var cell3 = row.insertCell(3);
-                            $(cell3).addClass('text-right');
-                            cell0.innerHTML = result.id;
-                            cell1.innerHTML = result.desc;
-                            cell2.innerHTML = "fecha";
-                            cell3.innerHTML =  `<button type="button" class="btn btn-primary btn-icon btn-sm" data-toggle="modal"
-                                                    data-target="#updateDescCatalogo" rel="tooltip" title="Actualizar la descripción">
-                                                    <i class="fa fa-pencil"></i>
-                                                </button>`;
+
+                            else if(actionType === "update"){
+                                $("#tdDesc"+id).text(desc);
+                                $('#updateDescCatalogo').modal('toggle');
+                            }
 			         	},
 			         	error: function(result) {
 				     		$.notify({
